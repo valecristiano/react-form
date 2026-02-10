01d7be046ddff65d87697e801f600b8ff075ef99
valecristiano <valeriamdcristiano@gmail.com>
2026-02-10 17:55:22 +0300

    Milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+const articlesList = ["Html", "Css", "Javascript"];
+
 export default function App() {
   return (
     <>
-      <h1>Welcome</h1>
+      <header className="bg-light">
+        <div className="container p-2">
+          <h1>Learn Web Development</h1>
+        </div>
+      </header>
+      <section className="container mt-4">
+        <ul className="list-group">
+          {articlesList.map((article) => (
+            <li className="list-group-item">{article}</li>
+          ))}
+        </ul>
+      </section>
     </>
   );
 }
