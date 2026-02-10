3b426c61cb6c45344a5b0c6e674ec58bab78a81c
valecristiano <valeriamdcristiano@gmail.com>
2026-02-10 19:04:48 +0300

    delete button bonus

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "./components/Header";
 
 const articlesList = ["Html", "Css", "Javascript"];
 
@@ -6,44 +7,53 @@ export default function App() {
   const [newTitleList, setNewTitle] = useState(articlesList);
   const [title, setTitle] = useState("");
 
+  const submitFunction = (e) => {
+    e.preventDefault();
+
+    if (title === "") return;
+
+    console.log(title);
+    setNewTitle([...newTitleList, title]);
+    setTitle("");
+  };
+
+  const inputFunction = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const deleteButtonFunction = (deletedIndex) => {
+    setNewTitle(newTitleList.filter((title, index) => index !== deletedIndex));
+  };
+
   return (
     <>
-      <header className="bg-light">
-        <div className="container p-2">
-          <h1>Learn Web Development</h1>
-        </div>
-      </header>
+      <Header></Header>
       {/* Lista Articoli */}
       <section className="container mt-4">
         <ul className="list-group">
           {newTitleList.map((article, index) => (
-            <li key={index} className="list-group-item">
-              {article}
+            <li key={index} className="list-group-item d-flex justify-content-between">
+              <p>{article} </p>{" "}
+              <button
+                onClick={() => {
+                  deleteButtonFunction(index);
+                }}
+                className="btn btn-outline-danger"
+              >
+                Elimina
+              </button>
             </li>
           ))}
         </ul>
       </section>
       {/* Sezione Input */}
       <section className="container mt-4">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(title);
-            setNewTitle([...newTitleList, title]);
-          }}
-        >
+        <form onSubmit={submitFunction}>
           <label className="mx-2" htmlFor="newtitle">
             Inserisci un nuovo titolo:
           </label>
-          <input
-            id="newtitle"
-            type="text"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <button className="btn btn-secondary mx-2">Aggiungi</button>
+          <input id="newtitle" type="text" value={title} onChange={inputFunction} />
+          <button className="btn btn-outline-success mx-2">Aggiungi</button>
         </form>
       </section>
     </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <>
+      <header className="bg-light">
+        <div className="container p-2">
+          <h1>Learn Web Development</h1>
+        </div>
+      </header>
+    </>
+  );
+}
